171419c7e8e91393a9bea383c3278f9472fa6db6
Omine <nozakimon@yahoo.co.jp>
2018-04-08 02:00:02 +0900

    modified

--- Title.h
+++ Title.h
@@ -2,31 +2,14 @@
 #include"Scene.h"
 #include"DxLib.h"
 
+class SceneManager;
+
 class Title : public Scene {
+	SceneManager *owner_;
 public:
-	Title() {
-		SetFontSize(BaseSize);
-	}
+	Title(SceneManager *scmgr);
+	Title();
 
-	void Draw() {
-		for (int i = 0; i < nHeight; i++) {
-			for (int j = 0; j < nWidth; j++) {
-				if (i == 0 || j == 0 || i == nHeight - 1 || j == nWidth - 1) {
-					int temp_x = BaseSize * j + 1;
-					int temp_y = BaseSize * i + 1; 
-					DrawBox(temp_x, temp_y, temp_x + BaseSize -1, temp_y + BaseSize -1, GREEN, false);
-				}
-			}
-		}
-	}
-
-
-	eScene Update() {
-
-		if (CheckHitKey(KEY_INPUT_RETURN)) {
-			return GAME;
-		}
-
-		return empty;
-	}
+	void Draw();
+	void Update();
 };
